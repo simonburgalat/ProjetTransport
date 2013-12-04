2506ca7e17819bd116da5ec4ef9ac3a2142cc893
simonburgalat <simon.burgalat@free.fr>
2013-12-04 18:04:24 +0100

    test

--- Cheminstab.php
+++ Cheminstab.php
@@ -29,23 +29,62 @@ catch (Exception $e)
 		<tr>
 		<td>
 		<?php
-		echo $donnees['id'];
+		echo $donnees['id']." : ";
+		$arr= explode("-",$donnees['Nom']);
+		$Nomreduit=$arr[0]."-".$arr[count($arr)-1];
+		echo $Nomreduit;
 		?>
 		
 		</td>
 		<td>
 		<?php
-		echo $donnees['Nom'];
+		$current_id=$donnees['id'];
+		
+		$reponse2 = $bdd->query("SELECT * FROM course WHERE Cheminref_id=$current_id");
+        $chaine= '';
+		$k=0;
+		while ($donnees2 = $reponse2->fetch())
+		{
+		
+		$chaine[$k]=$donnees2['Nom'].' - ';
+		$k++;
+		}
+				$chaine[$k-1]=substr($chaine[$k-1],0, strlen($chaine[$k-1])-2);
+				$list_course=implode("",$chaine);
+		echo $list_course;
+
+		
+		
+		
+		/*while ($donnees2 = $reponse2->fetch())
+		{
+		
+		$current_id=$donnees['Cheminref_id'];
+		if($current_id==$donnees['id'])
+		{
+		echo $donnees2['Nom'];
+		
+		
+		}
+		}
+		*/
+		
+		
+		
+		
+		
+		
 		?>
 		</td>
 		<!--TRAVAIL EN COURS : On va utiliser du JS et pas du PHP d'escroc comme la-->
-		<td>   <input type="submit" name="details_chemin" value="Détails" onclick="<?php include("courses_assoc.php"); ?>">
+		<td>   <input type="submit" name="details_chemin" value="Détails" onclick="">
 		</td>
 		</tr>
 		
 		<?php
 		}
 		$reponse->closeCursor();
+		$reponse2->closeCursor();
         ?>
  
    </body>
